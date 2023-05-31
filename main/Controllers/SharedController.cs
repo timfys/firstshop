067aa3ce51ec0b2485e3612cfd652e62f612aab2
timfy <timfy@bk.ru>
2023-05-31 11:17:05 +0300

    начал делать авторизацию :)))

---
+++ main/Controllers/SharedController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using main.Models;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace main.Controllers
+{
+    public class SharedController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly myContext _context;
+        private readonly User _user;
+
+        public SharedController(ILogger<HomeController> logger, myContext context, User user)
+        {
+            _context = context;
+            _logger = logger;
+            _user = user;
+        }
+        public IActionResult _Layout() => View();
+        [HttpPost]
+        public void Change([FromBody] TestModel model)
+        {
+            var user = _user.GetUser(model.Id);
+            _user.Update(user);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    } 
+}
--- main/Program.cs
+++ main/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using main.Models;
 using main;
